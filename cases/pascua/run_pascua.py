a07cd87b155cd33b48d02a81622d40bae5ff4326
JGM <jdgalaz@uc.cl>
2015-07-10 17:45:41 -0300

    ejemplo pascua

--- cases/pascua/run_pascua.py
+++ cases/pascua/run_pascua.py
@@ -52,14 +52,57 @@ def test_topo():
     plt.savefig('data/initv.png')
     plt.close()
     
+def makegauges(indir):
+  b=[[0, 1.00000, 1.00000],\
+    [24, 2.17500, 0.72000 ],\
+  [23, 2.51700, 0.79000 ],\
+  [22, 3.00300, 0.81000 ],\
+  [21, 3.61900, 0.78000 ],\
+  [20, 4.22400, 0.83000 ],\
+  [19, 4.83400, 0.95000 ],\
+  [18, 5.66500, 1.04000 ],\
+  [17, 6.27300, 1.03000 ],\
+  [16, 6.88400, 1.10000 ],\
+  [15, 7.49000, 1.50000 ],\
+  [14, 8.10100, 1.92000 ],\
+  [13, 8.71500, 2.04000 ],\
+  [12, 9.33100, 2.05000 ],\
+  [11, 9.93500, 2.21000 ],\
+  [10, 10.57200, 2.42000 ],\
+  [9, 11.17800, 2.68000 ],\
+  [8, 11.79100, 2.66000 ],\
+  [7, 12.40100, 2.44000 ],\
+  [6, 13.01100, 2.38000 ]]
+  f=open('%s/gauges.dat'%indir,'w')
+  f.write('%i\n'%len(b))
+  for i in range(len(b)):
+    f.write('%i %.18e %.18e\n'%(int(b[i][0]),b[i][1],b[i][2]))
+  f.close()
 
+def testgauges(indir):
+  g = np.loadtxt('%s/gauges.dat'%indir,skiprows=1)
+  x = np.loadtxt('%s/gridx.dat'%indir)
+  y = np.loadtxt('%s/gridy.dat'%indir)
+  z = np.loadtxt('%s/gridz.dat'%indir)
+  
+  plt.figure(figsize=(9.,4.))
+  plt.pcolormesh(x,y,z,cmap=plt.cm.gray,vmin=0.3,vmax=1.2)
+  plt.colorbar()
+  plt.axis('equal')
+  plt.scatter(g[:,1],g[:,2],s=10.,facecolor='w',edgecolor='none')
+  plt.xlim(x.min(),x.max())
+  for i in range(g.shape[0]):
+    plt.text(g[i,1],g[i,2],'%i'%g[i,0],fontsize=8,color='w')  
+  plt.savefig('%s/gauges.png'%indir,bbox_inches='tight')  
+  plt.close()
+  
 #--------parameters-------
 def setrun(nxi,neta,nn,npr,indir):
-  batifiles = ['%s/gridx.dat'%indir,'%s/gridy.dat'%indir,'%s/gridx.dat'%indir]
+  batifiles = ['%s/gridx.dat'%indir,'%s/gridy.dat'%indir,'%s/gridz.dat'%indir]
   initqfiles=['%s/inith.dat'%indir, '%s/initu.dat'%indir, '%s/initv.dat'%indir]
   caso=999
   tinit=0.
-  tfinal=1.
+  tfinal=10.
   cfl=0.95
   #nxi=300
   #neta=300
@@ -134,5 +177,7 @@ if __name__=='__main__':
   setrun(130,30,1,1,'data')
   maketopo()
   test_topo()
+  makegauges('data')
+  testgauges('data')
   
 
