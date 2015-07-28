a37d25efb790ca09f622a38b47d16077103ded86
JGM <jdgalaz@uc.cl>
2015-07-27 21:48:30 -0300

    test5 labokushiri ready
    laboratory experiment with reduced data (x[::4,::4])

---
+++ tests/test5_labokushiri/setrun.py
@@ -0,0 +1,191 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import os
+import urllib
+os.system('mkdir data')
+os.system('mkdir results')
+os.system('mkdir results/timeseries')
+
+#====================================
+#get original data from noaa website
+#=====================================
+
+rooturl='http://nctr.pmel.noaa.gov/benchmark/Laboratory/Laboratory_MonaiValley/'
+batifname0 = 'MonaiValley_Bathymetry.txt'
+wavefname0 = 'MonaiValley_InputWave.txt'
+if not os.path.isfile('data/'+batifname0):
+  urllib.urlretrieve(rooturl+batifname0,'data/'+batifname0)
+if not os.path.isfile('data/'+wavefname0):
+  urllib.urlretrieve(rooturl+wavefname0,'data/'+wavefname0)
+bati = np.loadtxt('data/MonaiValley_Bathymetry.txt',skiprows=1)
+wave = np.loadtxt('data/MonaiValley_InputWave.txt',skiprows=1)
+
+#=====================================
+#computational domain
+#=====================================
+
+ny = np.unique(bati[:,1]).shape[0]
+x = np.reshape(bati[:,0],(-1,ny))[::4,::4]
+y = np.reshape(bati[:,1],(-1,ny))[::4,::4]
+z = -1.*np.reshape(bati[:,2],(-1,ny))[::4,::4]
+
+dx = np.diff(x[:,0])[0]
+
+xwall = x[:,0].max()*np.ones((2,x.shape[1]))
+xwall[0,:] += dx
+xwall[1,:] *= 2*dx
+
+ywall = np.zeros_like(xwall)
+ywall[0,:] = y[-1,:]
+ywall[1,:] = y[-1,:]
+zwall = np.ones_like(xwall)*0.5
+
+x = np.vstack([x,xwall])
+y = np.vstack([y,ywall])
+z = np.vstack([z,zwall])
+
+
+h = np.where(z<=0.,-z,0.)
+u = np.zeros_like(x)
+v = np.zeros_like(x)
+
+
+plt.figure()
+plt.pcolormesh(x ,y ,z )
+plt.colorbar()
+plt.savefig('data/bati.png')
+
+plt.figure()
+plt.pcolormesh(x ,y ,h )
+plt.colorbar()
+plt.savefig('data/inith.png')
+
+plt.figure()
+plt.pcolormesh(x ,y ,h +z)
+plt.colorbar()
+plt.savefig('data/initeta.png')
+
+plt.figure()
+plt.pcolormesh(x ,y ,u )
+plt.savefig('data/initu.png')
+plt.colorbar()
+
+plt.figure()
+plt.pcolormesh(x ,y ,v )
+plt.savefig('data/initv.png')
+plt.colorbar()
+
+
+np.savetxt('data/gridx.dat',x )
+np.savetxt('data/gridy.dat',y )
+np.savetxt('data/gridz.dat',z )
+np.savetxt('data/inith.dat',h )
+np.savetxt('data/initu.dat',u )
+np.savetxt('data/initv.dat',v )
+
+#=====================================
+#incident wave
+#=====================================
+txi0 = wave[:,0]
+etaxi0 = np.zeros((x.shape[1],txi0.shape[0]))
+for i in range(x.shape[1]):
+  etaxi0[i,:] = wave[:,1]
+np.savetxt('data/timexi0.dat',txi0)
+np.savetxt('data/etaxi0.dat',etaxi0)
+#np.savetxt('data/etaL.dat',wave)
+
+plt.figure()  
+for i in range(etaxi0.shape[0]):
+    plt.plot(txi0,etaxi0[i,:])
+plt.savefig('data/etaxi0.png')    
+
+
+#=====================================
+#wave gauges
+#=====================================
+b=[[1,4.521, 1.196],
+   [2,4.521, 1.696],
+   [3,4.521, 2.196],
+   [4,0.000, 2.700],
+   [5,0.000, 1.700],
+   [6,0.000, 0.500],
+   [7,3.000, 2.700],
+   [8,3.000, 1.700],
+   [9,3.000, 0.500]]; 
+f=open('data/gauges.dat','w')
+f.write('%i\n'%len(b))
+for i in range(len(b)):
+  f.write('%i %.18e %.18e\n'%(b[i][0],b[i][1],b[i][2]))
+
+#=====================================
+#input.dat parameters
+#=====================================
+caso=999
+tinit=0.0
+tfinal=28.
+cfl=0.45
+nxi=x.shape[0]
+neta=x.shape[1]
+batiopt=1
+batifiles=['data/gridx.dat','data/gridy.dat','data/gridz.dat']
+initqopt=1
+initqfiles=['data/inith.dat', 'data/initu.dat', 'data/initv.dat']
+dxi=1.
+deta=1.
+L=1.
+H=1.
+U=1.
+bcxi0=4
+if bcxi0==4:
+  GA1=9
+  if GA1==9 or GA1==1:
+    Nsenal1=txi0.shape[0]
+bcxiN=1
+bceta0=1
+bcetaN=1
+dit=-1
+dtout = 0.5
+kappa=1e-5
+rktype=1
+limtype=1
+fricopt=0
+outopt=1
+outdir='results/'
+
+#---------write to file
+f=open('data/input.dat','w')
+f.write('%i'%caso)
+f.write('\n%.8f'%tinit)
+f.write('\n%.8f'%tfinal)
+f.write('\n%.8f'%cfl)
+f.write('\n%i'%nxi)
+f.write('\n%i'%neta)
+f.write('\n%i'%batiopt)
+for i in range(len(batifiles)):
+  f.write('\n%s'%batifiles[i])
+f.write('\n%i'%initqopt)
+for i in range(len(initqfiles)):
+  f.write('\n%s'%initqfiles[i])
+f.write('\n%.8f'%dxi)
+f.write('\n%.8f'%deta)
+f.write('\n%.8f'%L)
+f.write('\n%.8f'%H)
+f.write('\n%.8f'%U)
+f.write('\n%i'%bcxi0)
+if bcxi0==4:
+  f.write('\n%i'%GA1)
+  if GA1==9 or GA1==1:
+    f.write('\n%i'%Nsenal1)
+f.write('\n%i'%bcxiN)
+f.write('\n%i'%bceta0)
+f.write('\n%i'%bcetaN)
+f.write('\n%i'%dit)
+if dit==-1:
+  f.write('\n%.3f'%dtout)
+f.write('\n%3.16e'%kappa)
+f.write('\n%i'%rktype)
+f.write('\n%i'%limtype)
+f.write('\n%i'%fricopt)
+f.write('\n%i'%outopt)
+f.write('\n%s\n'%outdir)
+f.close()
---
+++ tests/test5_labokushiri/ver.py
@@ -0,0 +1,67 @@
+import numpy as np
+import matplotlib.pyplot as plt
+#import colormaps as cmaps
+from mpl_toolkits.axes_grid1 import make_axes_locatable
+outdir='results/'
+plotdir='vis/'
+#read parameters
+p=np.loadtxt('%s/param.dat'%outdir)
+caso=p[0]
+nxi=p[1]
+neta=p[2]
+nts=p[3]
+nit=p[4]
+dit=p[5]
+
+#time vector and geometry
+t=np.loadtxt('%s/time.dat'%(outdir))
+if int(dit)==-1:
+  sit=str(1).rjust(8,'0')
+  s=np.loadtxt('%s/SOL2D.%s.dat.gz'%(outdir,sit))
+else:
+  sit=str(0).rjust(8,'0')
+  s=np.loadtxt('%s/SOL2D.%s.dat.gz'%(outdir,sit))
+x=np.reshape(s[:,0],(nxi,neta),order='F')
+y=np.reshape(s[:,1],(nxi,neta),order='F')
+z=np.reshape(s[:,2],(nxi,neta),order='F')
+
+#loop
+rango=range(0,len(t)-1,1)
+#rango = np.nonzero(t>14.)[0]
+#rango=[0]
+x0=4.7
+xf=5.2
+y0=1.5
+yf=2.2
+for i in rango:
+  if int(dit)==-1:
+    sit=str(i+1).rjust(8,'0')
+    s=np.loadtxt('%s/SOL2D.%s.dat.gz'%(outdir,sit))
+  else:
+    sit=str(i*int(dit)).rjust(8,'0')
+    s=np.loadtxt('%s/SOL2D.%s.dat.gz'%(outdir,sit))
+    
+  h=np.reshape(s[:,3],(nxi,neta),order='F')
+  f=plt.figure()#figsize=(5.5,0.8*3.4))
+  ax=f.add_subplot(1,1,1)
+  p2=ax.pcolormesh(x,y,np.ma.masked_where(h<=1e-5,h+z),vmin=-0.05,vmax=0.05,cmap=plt.cm.coolwarm)
+  #ax.set_xticks(np.linspace(y0,yf,5))
+  #ax.set_xticklabels(['%.2f'%yi for yi in np.linspace(y0,yf,5)])
+  #ax.set_xlim(-yf,-y0)
+  #ax.set_ylim(x0,xf)
+  ax.set_xlabel('y[m]')
+  ax.set_ylabel('x[m]')
+  ax.set_aspect(1)
+  ax.set_title('T=%ss '%(t[i]))#,t[-1]))
+  divider = make_axes_locatable(ax)
+  cax = divider.append_axes("right", size="5%", pad=0.05)
+  plt.colorbar(p2, cax=cax)
+  ax.contour(x,y,z,np.arange(0.,0.2,0.009),colors='k')
+  ax.contour(x,y,z,np.arange(-0.04,0.,0.009),colors='k')
+  
+  f.tight_layout() 
+  #show()
+  print plotdir+'1dfig'+str(i).rjust(6,'0')+'.png'
+  plt.savefig(plotdir+'1dfig'+str(i).rjust(6,'0')+'.png',bbox_inches='tight')
+  plt.close()
+
